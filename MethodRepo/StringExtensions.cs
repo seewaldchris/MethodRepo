13e35986eed180b5cc9664ecf68aa06994177b13
seewaldchris <seewaldchris@gmail.com>
2014-03-25 08:57:29 -0400

    Added TrimManagedMetadata

---
+++ MethodRepo/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodRepo.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string TrimManagedMetadata(this string s)
+        {
+            if (s.Contains("|"))
+                return s.Substring(0, s.IndexOf('|'));
+            else
+                return s;
+        }
+    }
+}
